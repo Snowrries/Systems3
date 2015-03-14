261a3a24f218b46d36bdd2e31b62f74ee30c4729
aw449 <awong7905@gmail.com>
2015-03-14 02:21:51 -0400

    tokenizer.c errors
    There are a bunch of usage/syntax errors in tokenizer.c I have recorded
    them in as comments

--- tokenizer.c
+++ tokenizer.c
@@ -2,7 +2,7 @@
  * tokenizer.c
  */
 
-
+#include "tokenizer.h"
 void matching(const char *fds){	
 	regex_t patmat;/*Pattern matching*/
 	regmatch_t matmat;/*matchmat*/
@@ -12,8 +12,8 @@ void matching(const char *fds){
 	char *ofst;/*Offset*/
 	
 	char *inst; /*Insert value in burlapSack*/
-	bufflen* = 30;
-	desu = malloc()(bufflen*) * sizeof(char*));
+	*bufflen = 30;
+	desu = malloc((*bufflen) * sizeof(char*));
 	
 	b = fopen(fds,"r");
 	while(getline(desu, bufflen, b) > 0){
@@ -21,15 +21,23 @@ void matching(const char *fds){
 				if(regcomp(&patmat,REGEXPAT,REG_ICASE)!=0){
 					printf("ERROR, ERROR. Fix me please. And hurry up about it.");
 				}
-				ofst = (char *)malloc((1+strlen(b))*sizeof(char *));
+				//Passing Argument 1 of strlen incompatible pointer type
+				ofst = (char *) malloc((1+strlen(b))*sizeof(char ));
+				//Passing Argument 2 of strlen incompatible pointer type
+				//Incompatible type Argument 4 of regexec
 				if(regexec(&patmat, b, 1+strlen(b), matmat, 0)==0){
 						/*It matched~!*/
 						/*Here, we put the matches in the data struct. */
-						while(rm_so!=NULL){
-							inst = malloc((rm_eo - rm_so)*sizeof(char));
-							inst = b + rm_so;
+					//Comparison Between pointer and integer
+					//rm_so by itself is undefined
+						while(matmat.rm_so!=NULL){
+							inst = malloc((matmat.rm_eo - matmat.rm_so)*sizeof(char));
+							inst = b + matmat.rm_so;
 							/*Call the insert function!*/
-							InsertToken(RadNodeCreate(inst, strnlen(inst,em_eo)));
+							//NO YOU DON'T CALL INSERTTOKEN
+							//INSERTLOCATOR IS THE ONE TO USE
+
+							//InsertToken(RadNodeCreate(inst, strnlen(inst,matmat.rm_eo)));
 						}
 						/*End loop, line has been processed. */
 					}
