a9ad883830466ef43a6098a4972c33ad635ef127
Snowrries <allentung5@gmail.com>
2015-03-14 21:15:31 -0400

    sfd
    sfdgf

---
+++ count.c
@@ -0,0 +1,64 @@
+#include "count.h"
+
+hash HashCreate(int size){
+	hash kira;
+	int i;
+	kira = (hash)malloc(sizeof(hash));
+	kira->keys = (nodeptr*)malloc(size*sizeof(nodeptr*));
+	kira->rc = 0;
+	for(i = 0; i <size; i++){
+		kira->keys[i]=NULL;
+	}
+	return kira;
+}
+void AddValue(size_t address, hash hashtable){
+	int key;
+	nodeptr noo;
+	key = (int)(address%1000);
+	/*If the address is unique, add it.*/
+	if(exists(address, hashtable, key)==0){
+		noo = malloc(sizeof(nodeptr));
+		noo-> data = address;
+		noo-> next = hashtable->keys[key];
+		hashtable->keys[key] = noo;
+		hashtable->rc++;
+	}
+	/*else, do nothing.*/
+}
+
+int exists(size_t address, hash hashtable, int key){
+	nodeptr inc;
+	if(hashtable->keys[key] == NULL){
+		return 0;
+	}
+	inc = hashtable->keys[key];
+	while(inc->data != address){
+		if(inc->next == NULL){
+			return 0;
+		}
+		inc = inc->next;
+	}
+	return 1;
+}
+
+void freedom(hash hashtable, int size){
+	int i;
+	nodeptr del;
+	nodeptr keep;
+	for(i = 0; i < size; i++){
+		if((hashtable->keys)[i] !=NULL){
+			keep = (hashtable->keys)[i];
+			del = keep;
+			while(keep->next != NULL){
+				keep = keep->next;
+				free(del);
+				del = keep;
+			}
+			free(keep);
+		}
+	}
+	free(*(hashtable->keys));
+	free(hashtable);
+}
+
+
