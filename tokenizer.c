7697f00c4a7dd0d3fb7ce3a98e954732ff16c6a0
Snowrries <allentung5@gmail.com>
2015-03-14 23:20:13 -0400

    okay

---
+++ tokenizer.c
@@ -0,0 +1,88 @@
+#include "tokenizer.h"
+
+
+TokenizerT *TKCreate( char * filename ) {
+	char *desu;
+	size_t bufflen;
+	int getl;
+	TokenizerT *tk = (TokenizerT *)malloc(sizeof(TokenizerT));
+	if(tk==NULL){
+		return NULL;
+	}	
+	tk->boo = fopen(filename, "r");
+	tk->wholefile = malloc(sizeof(char));
+	tk->wholefile[0] = '\0';
+	while((getl = getline(&desu, &bufflen, tk->boo)) > 0){
+		tk->wholefile = realloc(tk->wholefile, strlen(tk->wholefile)+strlen(desu) );
+		strcat(tk->wholefile,desu);
+	}
+	
+	printf("Okay%s", tk->wholefile);
+	int regerrorcode = regcomp(&(tk->rx),REGEXPAT, REG_EXTENDED|REG_ICASE);
+	if (regerrorcode){
+		printf("well regcomp failed so here is the error code %d\n", regerrorcode);
+		return NULL;
+	}	
+	tk->untokened = (char *)malloc((strlen(tk->wholefile)+1) * sizeof(char *));
+	strcpy(tk->untokened, tk->wholefile);
+	tk->hasNext=regexec(&(tk->rx),tk->untokened, 1, &(tk->matches), 0);
+
+	tk->charsfromleft=(regoff_t)0;
+	tk->numtoken=0;
+	
+
+  return tk;
+}
+void TKDestroy( TokenizerT * tk ) {
+	free(tk->untokened);
+	fclose(tk->boo);
+	regfree(&(tk->rx));
+	//regfree(&(tk->keywordrx));
+	free(tk);
+}
+char *TKGetNextToken( TokenizerT * tk ) {
+		if(tk->hasNext){
+			return 0;
+		} 
+		int numchars=(int) tk->matches.rm_eo - (int)tk->matches.rm_so;
+		char *tokenz = (char *)malloc((numchars+1) * sizeof(char));
+		strncpy(tokenz, tk->untokened+tk->matches.rm_so+tk->charsfromleft, numchars);
+		tokenz[numchars]='\0';
+		regoff_t tempCharsholder = tk->matches.rm_eo;
+		tk->hasNext = regexec(&tk->rx,tk->untokened+tk->matches.rm_eo+tk->charsfromleft, 1, &tk->matches, REG_NOTBOL);
+		tk->charsfromleft+=tempCharsholder;
+		char *done = (char *)malloc((strlen(tokenz)+1)*sizeof(char)+1);
+		sprintf(done,"%s",  tokenz);
+		free(tokenz);
+		return done;
+
+
+}
+int TKhasNext(TokenizerT * tk){	
+	if(tk->hasNext == 0){
+		return 1;
+	}
+	return 0;
+}
+int main(int argc, char** argv){
+	FILE *fp;
+	char *token;
+   TokenizerT *stuff;
+   fp = fopen("/tmp/test.txt", "w+");
+   fprintf(fp, "This is testing for fprintf...\n");
+   fputs("This is testing for fputs...\n", fp);
+   fclose(fp);
+   stuff = TKCreate("/tmp/test.txt");
+	while(TKhasNext(stuff)){
+		token = TKGetNextToken(stuff);
+		if(token!=NULL){
+		printf("%s\n", token );
+		free(token);
+		}
+	}
+//  while((print = TKGetNextToken(stuff))!=0){
+//	   printf("%s", print);
+//   }
+   TKDestroy(stuff);
+  return 0;
+}
---
+++ tokenizer.h
@@ -0,0 +1,43 @@
+/*tokenizer.h*/
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <stdio.h>
+#include <regex.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "RadixTree.h"
+#define REGEXPAT "([a-z]([a-z]|[0-9])*)"
+
+
+struct TokenizerT_ {
+	//char * desu;
+	char * wholefile;
+	//size_t bufflen;
+	FILE* boo;
+	char* untokened;
+	regoff_t charsfromleft;
+	int numtoken;
+	regmatch_t matches;
+	regmatch_t matlol;
+	regex_t rx;
+	regex_t keywordrx;
+	int hasNext;
+	int wasModified;
+};
+
+typedef struct TokenizerT_ TokenizerT;
+
+
+TokenizerT *TKCreate( char * ts );
+
+void TKDestroy( TokenizerT * tk );
+
+char *TKGetNextToken( TokenizerT * tk );
+
+int TKhasNext(TokenizerT * tk);
+
+#endif //TOKENIZER_H
